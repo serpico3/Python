67146a63f6d1df5be9c74cbe7df50f421dab2110
Diego Serpelloni <121094870+serpico3@users.noreply.github.com>
2024-11-14 17:03:02 +0100

    primi esercizi

---
+++ Lezione_1.py
@@ -0,0 +1,5 @@
+lista = ['latte', 'pane', 'pasta', 'pomodori']
+print(lista)
+lista.append(input("Inserisci l'elemento da aggiungere: "))
+lista.remove(input("Inserisci l'elemento da rimuovere: "))
+print(lista)
---
+++ Lezione_2.py
@@ -0,0 +1,2 @@
+lista=[input("colore 1"),input("colore 2"),input("colore 3")]
+#print(lista,lista[0])
