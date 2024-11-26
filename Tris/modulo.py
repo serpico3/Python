e7c7614664b8244701565b5490c1d704fa61c0bb
Diego Serpelloni <121094870+serpico3@users.noreply.github.com>
2024-11-26 15:34:12 +0100

    non so come funziona

---
+++ Tris/modulo.py
@@ -0,0 +1,28 @@
+def printRes1(n):
+    print(1)
+    n = 1
+    return n 
+def printRes2():
+    print(2)
+def printRes3():
+    print(3)
+def printRes4():
+    print(4)
+def printRes5():
+    print(5)
+def printRes6():
+    print(6)
+def printRes7():
+    print(7)
+def printRes8():
+    print(8)
+def printRes9():
+    print(9)
+def add(n):
+    print("+")
+def sub(n):
+    print("-")
+def mul(n):
+    print("*")
+def div(n):
+    print("/")
